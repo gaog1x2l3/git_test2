c8b9df1c499056a7d35e35e9b90a0c39afb6bc94
gaog1x2l3 <g1x2l3@126.com>
2016-05-24 10:55:34 +0800

    克隆后的修改，首次修改

--- app/src/main/java/mobiletrain/org/git_test2/MainActivity.java
+++ app/src/main/java/mobiletrain/org/git_test2/MainActivity.java
@@ -10,5 +10,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ////////
+        System.out.println("克隆后的修改");
+
     }
 }
